342beb57ba777c84252ab7f9710f584c73f37237
Kimberly miller <135668982+Kmiller105@users.noreply.github.com>
2025-09-13 17:04:37 -0500

    update

--- loops.js
+++ loops.js
@@ -1,1 +1,19 @@
-
+const peoples = ['kim','josh','makenzie','brayden','colton','madison'];
+for (people of peoples ){
+    console.log(`We are family ${peoples}`);
+    }
+    const text =document.querySelector(".title");
+const changecolor = document.querySelector(".click");
+text.style.backgroundColor = "blue";
+text.classList.add("redo");
+changecolor.addEventListener('click', function(){
+text.classList.toggle("redo");
+});
+const userlist = document.querySelectorAll('.animal-list li');
+console.log(userlist);
+for(user of userlist){
+    user.addEventListener('click', function(){
+        console.log(this);
+        this.style.color = "plum";
+    });
+}
